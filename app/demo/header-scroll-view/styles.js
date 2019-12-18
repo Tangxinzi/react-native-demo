f3cc24ec7fb0e8aefce1db8f4813bb37fc45cc79
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-18 18:21:59 +0800

    Header Scroll View

---
+++ app/demo/header-scroll-view/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from 'react-native';
+import { ifIphoneX } from 'react-native-iphone-x-helper';
+
+const headerHeight = ifIphoneX(88, 60);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent'
+  },
+  headerContainer: {
+    height: headerHeight,
+  },
+  headerComponentContainer: {
+    height: headerHeight,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 12,
+  },
+  headline: {
+    fontSize: 17,
+    lineHeight: 22,
+    fontWeight: '500',
+    letterSpacing: 0.019,
+  },
+  title: {
+    letterSpacing: 0.011,
+    fontWeight: '700',
+    marginLeft: 16,
+  },
+});
+
+export default styles;
--- index.js
+++ index.js
@@ -3,7 +3,8 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import App from './App';
+import App from './app/demo/header-scroll-view/Root'
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
