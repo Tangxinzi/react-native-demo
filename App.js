402ca390f9f78cce297bd10400e251e9e7c381a7
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-24 17:38:09 +0800

    react-native-root-toast

--- App.js
+++ App.js
@@ -24,6 +24,36 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+import Toast from 'react-native-root-toast';
+
+let toast = Toast.show('保存成功', {
+  duration: 2000, // toast 显示时长
+  position: Toast.positions.BOTTOM, // toast 位置
+  shadow: true, // toast 是否出现阴影
+  animation: true, // toast 显示/隐藏的时候是否需要使用动画过渡
+  hideOnPress: true, // 是否可以通过点击事件对 toast 进行隐藏
+  backgroundColor: '#000000',
+  textColor: '#ffffff',
+  delay: 0, // toast 显示的延时
+  onShow: () => {
+    // toast 出现回调（动画开始时）
+  },
+  onShown: () => {
+    // toast 出现回调（动画结束时）
+  },
+  onHide: () => {
+    // toast 隐藏回调（动画开始时）
+  },
+  onHidden: () => {
+    // toast 隐藏回调（动画结束时）
+  }
+});
+
+// You can manually hide the Toast, or it will automatically disappear after a `duration` ms timeout.
+// setTimeout(function () {
+//     Toast.hide(toast);
+// }, 5000);
+
 const App: () => React$Node = () => {
   return (
     <>
