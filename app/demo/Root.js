4471d6f50e791210540ddf9f58c45d99c1d56bc4
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2019-12-25 17:46:57 +0800

    Drawer

---
+++ app/demo/Root.js
@@ -0,0 +1,18 @@
+/**
+ * React Native Demo by ninghao.net
+ * https://github.com/ninghao/NinghaoReactNative
+ */
+'use strict';
+import React from 'react';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
+import DrawerDemo from './ant-design/Drawer';
+
+const AntDesignReactNative: () => React$Node = () => {
+    return (
+      <>
+        <DrawerDemo />
+      </>
+    );
+}
+
+export default AntDesignReactNative
